c59304a8c6415cbbe5ead1f91d49b00299f1c4b9
DSTG <Admin@S-DG0-CTM-P002.gcm.local>
2025-11-22 09:57:19 +0100

    Affiche la liste des favoris

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+// Chargement des favoris depuis LocalStorage
 let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
 
 const resultsGrid = document.getElementById('resultsGrid');
@@ -6,3 +6,12 @@ const favoritesGrid = document.getElementById('favoritesGrid');
 const loader = document.getElementById('loader');
 const searchInput = document.getElementById('searchInput');
 const searchButton = document.getElementById('searchButton');
+
+// Affiche la liste des favoris
+function updateFavorites() {
+  favoritesGrid.innerHTML = '';
+  if (favorites.length === 0) {
+    favoritesGrid.innerHTML = '<p>Aucun favori pour le moment.</p>';
+    return;
+  }
+  favorites.forEach(async id => {
